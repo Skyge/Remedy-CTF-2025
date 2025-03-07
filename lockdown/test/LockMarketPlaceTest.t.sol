13ce32f06586ee51f371332359d2989af3fa9d27
Skyge <1506186404li@gmail.com>
2025-03-07 10:10:02 +0800

    :zap: Update the solution for the lockdown.

--- lockdown/test/LockMarketPlaceTest.t.sol
+++ lockdown/test/LockMarketPlaceTest.t.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.0;
 import {Test, console2} from "forge-std/Test.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Challenge} from "src/Challenge.sol";
-import {LockMarketplace} from "src/LockMarketplace.sol";
-import {LockToken} from "src/LockToken.sol";
 import {Exploit} from "src/solution/Exploit.sol";
 
 contract LockMarketPlaceTest is Test {
+    Exploit internal exploit;
     Challenge internal challenge;
     IERC20 internal usdc;
-    Exploit internal exploit;
 
     function setUp() external {
         vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 21_690_640);
@@ -20,14 +18,20 @@ contract LockMarketPlaceTest is Test {
         challenge = new Challenge((address(exploit)));
         
         usdc = challenge.USDC();
+        
         // Distribute 1_000_520e6 USDC to the challenge contract
         deal(address(usdc), address(challenge), 1_000_520e6);
         // Deploy the whole system
         challenge.deploy();
-    }
+
+        // Transfer 500 USDC to the exploit contract
+        vm.startPrank(challenge.PLAYER());
+        usdc.transfer(address(exploit), 500e6);
+        vm.stopPrank();
+    } 
 
     function testExploit() external {
         exploit.pwn(challenge);
-        assert(challenge.isSolved());
+        assertTrue(challenge.isSolved());
     }
 }
